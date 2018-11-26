515ac03b4b9293409ac77b0b598c993330a52314
ElviraMoa <43365867+ElviraMoa@users.noreply.github.com>
2018-11-26 20:18:50 +0300

    Шаг 1.
    Подключены библиотеки, что понадобятся для работы с программой: string - для работы со строками, iostream - стандартная библиотека с функциями, fstream - для работы с потоками, sstream - для работы со строками через поток. Обозначены переменные: line - слово и search - слово для поиска.

---
+++ Laba2/main.cpp
@@ -0,0 +1,13 @@
+#include <string>
+#include <iostream>
+#include <fstream>
+#include <sstream>
+
+using namespace std;
+
+int main() {
+	setlocale(LC_ALL, "");
+	ifstream in("input.txt");
+	string line;
+	string search; 
+	cout << "¬ведите слово дл€ поиска: ";
