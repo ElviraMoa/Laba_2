7f3c2762bf073a2de0a9beabee1618cbe107edd5
ElviraMoa <43365867+ElviraMoa@users.noreply.github.com>
2018-11-27 04:08:47 +0300

    Шаг 3.
    Добавлен цикл считывания по предложению. Добавили условие, при котором в буфере у предложения убираем последний символ, так как это может быть точка или что-либо еще, и добавили условие выхода из цикла при конце предложения ("/n")

--- Laba2/main.cpp
+++ Laba2/main.cpp
@@ -9,8 +9,22 @@ int main() {
 	setlocale(LC_ALL, "");
 	ifstream in("input.txt");
 	string line;
-	string search; 
+	string search;
 	cout << "¬ведите слово дл€ поиска: ";
 	cin >> search;
-	stringstream ss;	
+	stringstream ss;
 	int cnt = 0;
+	while (in >> line) {
+		ss << line << ' ';
+		string temp = line;
+		temp[temp.size() - 1] = 0;
+		if (line == search || temp == search) {
+			++cnt;
+		}
+		if (line[line.size() - 1] == '.') {
+			ss << " count = " << cnt << '\n';
+			cnt = 0;
+		}
+
+	}
+}
